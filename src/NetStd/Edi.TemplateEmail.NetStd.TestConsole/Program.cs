63a81ca36996220cab3498aaa568a1db720baa7d
Edi Wang <Edi.Wang@outlook.com>
2018-06-08 12:42:45 +0800

    add .net standard projects

---
+++ src/NetStd/Edi.TemplateEmail.NetStd.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Edi.TemplateEmail.NetStd.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
